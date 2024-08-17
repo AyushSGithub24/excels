8caa9865a66a38a5840953eaa0acbd06a4db4770
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-08-17 15:29:46 +0530

    first commit

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Spreadsheet from "react-spreadsheet";
+import "./Home.css";
 
 export const Home = () => {
   const [data, setData] = useState([
@@ -26,9 +27,20 @@ export const Home = () => {
    
   ]);
   return (
-    <div>
+    // <div>
       
-      <Spreadsheet data={data} onChange={setData} />
-    </div>
+    //   <Spreadsheet data={data} onChange={setData} />
+    // </div>
+    <div className="spreadsheet-container">
+    <Spreadsheet
+      data={data}
+      onChange={setData}
+      cellRenderer={({ children }) => (
+        <div className="spreadsheet-cell">
+          {children}
+        </div>
+      )}
+    />
+  </div>
   )
 };
