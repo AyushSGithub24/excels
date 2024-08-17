4e813015494ebb9012f0a0912e0d6efd8a750086
yashasvinitripathi11 <yashasvinitripathi@gmail.com>
2024-08-17 14:45:30 +0530

    first commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import Spreadsheet from "react-spreadsheet";
+
+export const Home = () => {
+  const [data, setData] = useState([
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}],
+    [{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}, {value: ""},{value: ""}]
+   
+  ]);
+  return (
+    <div>
+      
+      <Spreadsheet data={data} onChange={setData} />
+    </div>
+  )
+};
